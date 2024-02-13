8b69878ad80c4e13fb002745f3280d9acde31728
0GiS0 <giselatb@outlook.com>
2024-02-14 00:22:25 +0100

    Add Gum instructions and update active_commiters.sh script

---
+++ README.md
@@ -0,0 +1,7 @@
+# Create an interactive terminal usign Gum
+
+# 1. Install Gum
+brew install gum
+
+# 2. Create a new Gum project
+gum new my_project
--- active_commiters.sh
+++ active_commiters.sh
@@ -1,11 +1,41 @@
-# Estimate the committers that would be added to the customer's usage if Advanced Security was enabled for this organization.
-ADO_ORGANIZATION="returngisorg"
-ADO_PERSONAL_ACCESS_TOKEN=$1
+#!/bin/sh
 
-# Org Meter Usage Estimate
-curl -u :$ADO_PERSONAL_ACCESS_TOKEN -X GET \
--H "Accept: application/json" \
-"https://advsec.dev.azure.com/$ADO_ORGANIZATION/_apis/management/meterUsageEstimate?api-version=7.2-preview.1"
+gum style \
+	--foreground 212 --border-foreground 212 --border double \
+	--align center --width 50 --margin "1 2" --padding "2 4" \
+	'Welcome to GHAzDO investigation!' 'Check the active commiters in your Azure DevOps organization, project or repository.'
+
+ADO_PERSONAL_ACCESS_TOKEN="4g4uyxx2byevtovmpjoccqijg7ay4vj73rbpys7ldk2fd4pbhrga"
+
+echo "At which level do you want to get the active commiters?"
+CHOICE=$(gum choose "Organization" "Project" "Repository")
+
+if [ "$CHOICE" = "Organization" ]; then
+    echo "You chose Organization"
+
+    ORG_NAME=$(gum input --placeholder="Enter your Azure DevOps Organization Name")
+
+    echo "Thinking..."
+    
+
+    # Org Meter Usage Estimate
+    gum spin --spinner dot --title "Thinking..." -- curl -u :$ADO_PERSONAL_ACCESS_TOKEN -X GET \
+            -s \
+            -H "Accept: application/json" \
+            "https://advsec.dev.azure.com/$ORG_NAME/_apis/management/meterUsageEstimate?api-version=7.2-preview.1" | jq '.count'
+
+    echo "Active Commiters for $ORG_NAME: $COUNT"
+
+elif [ "$CHOICE" = "Project" ]; then
+    echo "You chose Project"
+else
+    echo "You chose Repository"
+fi
+
+PAT=$(gum input --placeholder="Enter your Azure DevOps Personal Access Token" --password)
+
+
+
 
 # Get the list of projects in the organization
 curl -u :$ADO_PERSONAL_ACCESS_TOKEN -X GET \

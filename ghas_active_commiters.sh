02af5c81297801c982176e49135ce51c78d5e3d4
0GiS0 <giselatb@outlook.com>
2024-05-28 11:21:12 +0200

    chore: Refactor active_commiters.sh script and update GHAS API calls

--- ghas_active_commiters.sh
+++ ghas_active_commiters.sh
@@ -1,15 +1,17 @@
 gh auth login
 
+ORG=returngis
+
 gh api \
   -H "Accept: application/vnd.github+json" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \  
-  /orgs/returngis/settings/billing/advanced-security
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  /orgs/$ORG/settings/billing/advanced-security
 
 gh auth refresh -h github.com -s admin:enterprise
 
 gh auth refresh -h github.com -s manage_billing:enterprise
 
-ENTERPRISE_NAME=<YOUR_ENTERPRISE_NAME>
+ENTERPRISE_NAME=returngis-enterprise
 
 gh api \
   -H "Accept: application/vnd.github+json" \
